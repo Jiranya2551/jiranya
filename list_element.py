0aa1f0098ade4e0e0b55a0c10d6b665138e88d0e
Jiranya2551 <116702716+Jiranya2551@users.noreply.github.com>
2022-11-23 12:15:46 +0700

    Add files via upload

---
+++ list_element.py
@@ -0,0 +1,8 @@
+goals = ['call to grandam','shopping','cook dinner']
+print(goals)
+i = int (input('whichgoal do you want to chage? ' ))
+goals[i] = input("enter a new goal: ")
+print(goals)
+#i = input('which goal do you want to print?: ')
+#i = int(i)# convert letter(str) to the numder (integer)
+#print(goals[i])
