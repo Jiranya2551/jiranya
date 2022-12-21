ad3791d8ebbdeab7db00044607a3e35ae43be335
Jiranya2551 <116702716+Jiranya2551@users.noreply.github.com>
2022-12-21 12:06:21 +0700

    Add files via upload

---
+++ teamlist = [].py
@@ -0,0 +1,8 @@
+teamlist = []
+
+for i in range(11):
+    name = input("Tom","jen","oil","adam","peter","emliy","emma","marilyn","karen","alisha")
+    teamlist.append(name)
+
+for player in teamlist:
+    print(player)
